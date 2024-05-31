2ccb0e914d51ea6e64d7969eaf3afb5f9d0c41a2
Tiago <ti.almeida58@gmail.com>
2024-05-31 09:40:47 -0300

    feat: resolving TASK5

--- myinterview/src/main/java/com/example/TASK5.java
+++ myinterview/src/main/java/com/example/TASK5.java
@@ -1,11 +1,23 @@
 package com.example;
 
+
 /**
  * Create an implementation of a Rest API .
  * Expose an API. Feel free to explore possibilities/functionalities/capabilities following Rest standard.
  * We suggest that your implementation have at least a CRUD scenario.
  *
  */
-public class TASK5 {
 
+public class TASK5 {
+    /**
+     * Criei um CRUD de employees usando clean architecture
+     * Salva: http://localhost:8080/employee
+     * Update: http://localhost:8080/employee
+     * Delete: http://localhost:8080/employee
+     * GEt All: http://localhost:8080/employee
+     * Get By Id: http://localhost:8080/employee/{id}
+     * GET By FirstName: http://localhost:8080/employee/firstName/{firstName}
+     * GEt By LastName: http://localhost:8080/employee/lastName/{lastName}
+     * GEt By Gender: http://localhost:8080/employee/gender/{gender}
+     */
 }
