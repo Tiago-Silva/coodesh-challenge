4709c3581a1e38ac3ffc206999eda5118cddaffc
Tiago <ti.almeida58@gmail.com>
2024-05-31 10:23:05 -0300

    raw: creating .env

--- myinterview/src/main/java/com/example/MyinterviewApplication.java
+++ myinterview/src/main/java/com/example/MyinterviewApplication.java
@@ -2,11 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class MyinterviewApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("spring.datasource.url", Objects.requireNonNull(dotenv.get("DB_URL")));
+        System.setProperty("spring.datasource.username", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+        System.setProperty("spring.datasource.password", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+
         SpringApplication.run(MyinterviewApplication.class, args);
     }
 }
